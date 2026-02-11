77c28af392ea20e82ccef2b6ffaa4e9df9fde51b
Matthew Burlew <matthew.burlew@student.fairfield.edu>
2026-02-11 11:36:15 -0500

    Update Assignment 2 Q 2.py

--- Assignment 2 Q 2.py
+++ Assignment 2 Q 2.py
@@ -2,16 +2,18 @@ salary = float(input("Enter your annual salary: "))
 score = int(input("Enter your performance score (0-100): "))
 
 if score >= 90:
-    bonus_percentage = 0.10
+    bonus_percentage = 0.20
 elif score >= 80:
     bonus_percentage = 0.10
 elif score >= 70:
     bonus_percentage = 0.05
-else: 
+else:
     bonus_percentage = 0.00
 
-bonus_amout = salary *bonus_percentage
+bonus_amount = salary * bonus_percentage
 
-print("Performance Bonus: ", bonus_percentage*100, "%")
-print("Bonus Amount: $", round(bonus_amout, ))
+print("Performance Bonus:", int(bonus_percentage * 100), "%")
+print(f"Bonus Amount: ${bonus_amount:.2f}")
+
+
 
